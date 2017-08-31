7b698aecc850e72b1cf7a612f91880552acbac1f
Oscar Perez <oscarfernando.perez@gmail.com>
2017-08-31 15:09:59 +0200

    Changes

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -1,19 +1,7 @@
 #!/usr/bin/groovy
 def call() {
-
-    def dockerImage =  'docker:1.11'
-
-    podTemplate(label: label,
-            containers: [
-                    [name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,  workingDir: '/home/jenkins/',
-                     envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]]],
-            volumes: [
-                    secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                    secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) 
-    {
-        body()
-    }
-
+podTemplate(label: label,
+        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
+        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+    body()
 }
--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -1,21 +1,8 @@
 #!/usr/bin/groovy
 def call() {
-
-    def mavenImage =  'maven'
-  
-    podTemplate(label: label,
-            containers: [
-                     [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
-                     envVars: [
-                             [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
-            volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                      persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo'),
-                      secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                      secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                      secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
-                      hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']])
-      {
-        body()
-      }
+podTemplate(label: label,
+        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'cat', ttyEnabled: true)],
+        volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
+                  persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo')]) {
+    body()
 }
