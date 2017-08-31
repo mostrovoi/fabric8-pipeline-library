15318af157ddb6979727071908be69b2866d852d
Oscar Perez <oscarfernando.perez@gmail.com>
2017-08-31 14:46:07 +0200

    changes

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -1,25 +1,17 @@
 #!/usr/bin/groovy
-def call(Map parameters = [:], body) {
+def call() {
 
-    def defaultLabel = 'docker'
-    def label = parameters.get('label', defaultLabel)
-
-    def dockerImage = parameters.get('dockerImage', 'docker:1.11')
-    def inheritFrom = parameters.get('inheritFrom', 'base')
-    def jnlpImage =  'jenkinsci/jnlp-slave:2.62'
-    def cloud = "kubernetes"
-
+    def dockerImage =  'docker:1.11'
 
     podTemplate(label: label,
             containers: [
-                    //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}',  workingDir: '/home/jenkins/'],
                     [name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,  workingDir: '/home/jenkins/',
-                     envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/'],[key: 'DOCKER_API_VERSION', value: '1.23']]]],
+                     envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]]],
             volumes: [
                     secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                     secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                     hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/'],[key: 'DOCKER_API_VERSION', value: '1.23']]) {
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) {
 
         body()
 
--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -1,22 +1,16 @@
 #!/usr/bin/groovy
 
-def call(Map parameters = [:], body) {
+def call() {
 
-    def defaultLabel = "maven"
-    def label = parameters.get('label', defaultLabel)
-
-    def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:2.2.297')
-    def jnlpImage =  'jenkinsci/jnlp-slave:2.62'    
+    def mavenImage =  'maven'
+  
     podTemplate(label: label,
             containers: [
-                    //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
-                    [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
+                     [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
                      envVars: [
                              [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
             volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                      persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                      secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                      secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
+                      persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo'),
                       secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                       secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
                       secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
