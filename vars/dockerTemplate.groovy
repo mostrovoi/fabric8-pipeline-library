e34242ddc8a2df149ab93006a50dd097d0feb4d1
Oscar Perez <oscarfernando.perez@gmail.com>
2017-08-31 15:11:11 +0200

    changes

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -4,4 +4,5 @@ podTemplate(label: label,
         containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     body()
+  }
 }
