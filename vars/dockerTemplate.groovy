5e14815d81985b487b0aff6118c86c3702d37329
Oscar Perez <oscarfernando.perez@gmail.com>
2017-09-01 14:06:35 +0200

    pipes

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -6,10 +6,10 @@ def call(Map parameters = [:], body) {
 
     podTemplate(label: label, inheritFrom: 'base',
             containers: [
-                     [name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,  workingDir: '/home/jenkins/',
-                     envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/'],[key: 'DOCKER_API_VERSION', value: '1.23']]]],
+                         [name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,  workingDir: '/home/jenkins/']
+                        ],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/'],[key: 'DOCKER_API_VERSION', value: '1.23']]) 
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock']]) 
     {
         body()
     }
