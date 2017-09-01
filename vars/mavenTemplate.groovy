9a779d022d43f4f355ad178e29313390f07eee0a
Oscar Perez <oscarfernando.perez@gmail.com>
2017-09-01 09:34:54 +0200

    changes

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -14,12 +14,7 @@ def call(Map parameters = [:], body) {
                     [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
                      envVars: [
                              [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
-            volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                      secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                      secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                      secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                      secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                      secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
+            volumes: [persistentVolumeClaim(claimName: 'maven-repo', mountPath: '/root/.mvnrepository'),
                       hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
     ) {
