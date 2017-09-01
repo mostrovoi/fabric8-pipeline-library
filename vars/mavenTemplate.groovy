e7112596c81b0abc7d91bfbc9ee21834cd622ffc
Oscar Perez <oscarfernando.perez@gmail.com>
2017-09-01 09:38:36 +0200

    Right image

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -3,7 +3,7 @@ def call(Map parameters = [:], body) {
 
     def label = parameters.get('label', 'maven')
 
-    def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:2.2.297')
+    def mavenImage = parameters.get('mavenImage', 'maven')
 
     // 0.13 introduces a breaking change when defining pod env vars so check version before creating build pod
    
@@ -14,7 +14,7 @@ def call(Map parameters = [:], body) {
                     [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
                      envVars: [
                              [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
-            volumes: [persistentVolumeClaim(claimName: 'maven-repo', mountPath: '/root/.mvnrepository'),
+            volumes: [persistentVolumeClaim(claimName: 'maven-repo', mountPath: '/root/.m2/repository'),
                       hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
     ) {
