282894cd52320fb48cc0981ddda51e3492e23a2a
Oscar Perez <oscarfernando.perez@gmail.com>
2017-09-01 09:07:10 +0200

    Fix for pvc

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -15,7 +15,7 @@ def call(Map parameters = [:], body) {
                      envVars: [
                              [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
             volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                      persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
+                      persistentVolumeClaim(claimName: 'maven-repo', mountPath: '/root/.mvnrepository'),
                       secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                       secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                       secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
