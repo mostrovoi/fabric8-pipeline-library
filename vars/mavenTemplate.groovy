2f6c3690b2daa8a42f4b9900dca0bdd5cfbd1147
Oscar Perez <oscarfernando.perez@gmail.com>
2017-09-01 09:23:11 +0200

    Changes

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -3,7 +3,7 @@ def call(Map parameters = [:], body) {
 
     def label = parameters.get('label', 'maven')
 
-    def mavenImage = parameters.get('mavenImage', 'maven')
+    def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:2.2.297')
 
     // 0.13 introduces a breaking change when defining pod env vars so check version before creating build pod
    
@@ -11,7 +11,18 @@ def call(Map parameters = [:], body) {
 
     podTemplate(label: label, inheritFrom: 'base',
             containers: [
-                    [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true]]
+                    [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
+                     envVars: [
+                             [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
+            volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
+                      persistentVolumeClaim(claimName: 'maven-repo', mountPath: '/root/.mvnrepository'),
+                      secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                      secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
+                      secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                      secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
+                      secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
+                      hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
     ) {
 
         body(
