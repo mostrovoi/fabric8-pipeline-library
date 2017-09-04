6db9222ff056a4941adc740d6d0c98649fcedb12
Oscar Perez <oscarfernando.perez@gmail.com>
2017-09-04 15:44:43 +0200

    deploy

---
+++ vars/deployProject.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/groovy
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def projectName = config.stagedProject[0]
+  def releaseVersion = config.stagedProject[1]
+
+  def rc = readFile config.resourceLocation
+  
+  kubernetesApply(file: rc, environment: config.environment)
+
+}
