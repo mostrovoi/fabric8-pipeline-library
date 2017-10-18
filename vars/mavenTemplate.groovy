ed8287cf4dd07717cc2261109b4902874e5f2a81
Oscar <oscar.perez_gov.ext@gencat.cat>
2017-10-18 17:34:54 +0200

    Fix for automatic handling of image naming

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -3,10 +3,10 @@ def call(Map parameters = [:], body) {
 
     def label = parameters.get('label', 'maven')
     def mavenImage = parameters.get('mavenImage', 'maven')
-    def nameContainer = parameters.get('name','maven')
+    def nameImage = parameters.get('nameImage','maven')
     podTemplate(label: label, inheritFrom: 'base',
             containers: [
-                    [name: '${nameContainer}', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
+                    [name: '${nameImage}', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
                      envVars: [
                              [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']]]],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
