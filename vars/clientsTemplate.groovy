cda8ef3735ba0ba97b8d47b6f421fac499436103
Oscar <oscar.perez_gov.ext@gencat.cat>
2017-10-25 17:23:18 +0200

    Adding body as parameter

--- vars/clientsTemplate.groovy
+++ vars/clientsTemplate.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 //def call(Map parameters = [:], body) {
-def call() {
+def call(body) {
     //def label = parameters.get('label', "clients")
     //def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
 
@@ -10,6 +10,6 @@ def call() {
             volumes: [         
                     hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) {
-        //body()
+        body()
     }
 }
