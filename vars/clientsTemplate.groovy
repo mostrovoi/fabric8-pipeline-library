cebdb083630959f7f637c8fe6e974c9008b9b93b
Oscar <oscar.perez_gov.ext@gencat.cat>
2017-10-25 16:17:01 +0200

    Changes to clientsTemplate

--- vars/clientsTemplate.groovy
+++ vars/clientsTemplate.groovy
@@ -2,15 +2,15 @@
 def call(Map parameters = [:], body) {
 
     def label = parameters.get('label', "clients")
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:0.14')
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
 
-    podTemplate(label: label, inheritFrom: "base",
+    podTemplate(cloud: cloud, label: label, serviceAccount: 'jenkins', inheritFrom: "base",
             containers: [
-                            [name: 'clients', image: "${clientsImage}", command: '/bin/sh -c', args: 'cat', privileged: true,  workingDir: '/home/jenkins/', ttyEnabled: true, workingDir: '/home/jenkins/']
+                            [name: 'clients', image: "${clientsImage}", command: '/bin/sh -c', args: 'cat', privileged: true,  workingDir: '/home/jenkins/', ttyEnabled: true, workingDir: '/home/jenkins/', envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]]
                         ],
             volumes: [         
                     hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock']]) {
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) {
         body()
     }
 }
