4653c5a1b2a54b3858fd4d6cb96232ea4c4936bd
Oscar <oscar.perez_gov.ext@gencat.cat>
2017-10-25 16:52:20 +0200

    Changes in syntax

--- vars/clientsTemplate.groovy
+++ vars/clientsTemplate.groovy
@@ -5,8 +5,7 @@ def call(Map parameters = [:], body) {
     def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
 
     podTemplate(cloud: 'kubernetes', label: label, serviceAccount: 'jenkins', inheritFrom: "base",
-            containers: [
-                            [name: 'clients', image: "${clientsImage}", command: '/bin/sh -c', args: 'cat', privileged: true,  workingDir: '/home/jenkins/', ttyEnabled: true, workingDir: '/home/jenkins/', envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]]
+            containers: [containerTemplate(name: 'clients', image: "${clientsImage}", command: '/bin/sh -c', args: 'cat', privileged: true,  workingDir: '/home/jenkins/', ttyEnabled: true, workingDir: '/home/jenkins/', envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']])
                         ],
             volumes: [         
                     hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -4,9 +4,8 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', 'docker')
     def dockerImage = parameters.get('dockerImage', 'docker:1.11')
 
-    podTemplate(cloud: 'kubernetes', label: label, serviceAccount: 'jenkins', inheritFrom: 'base',
-            containers: [
-                         [name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,  workingDir: '/home/jenkins/']
+    podTemplate(label: label, inheritFrom: 'base',
+            containers: [containerTemplate(name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,  workingDir: '/home/jenkins/')
                         ],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock']]) 
