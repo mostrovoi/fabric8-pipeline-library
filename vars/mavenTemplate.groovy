43b7e6867ec6cd29d471dbae100bbfb51cae2769
Oscar <oscar.perez_gov.ext@gencat.cat>
2017-10-26 12:20:46 +0200

    Change to right syntax

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -5,10 +5,9 @@ def call(Map parameters = [:], body) {
     def mavenImage = parameters.get('mavenImage', 'maven')
     def nameImage = parameters.get('nameImage','maven')
     podTemplate(label: label, inheritFrom: 'base',
-            containers: [
-                    [containerTemplate(name: "${nameImage}", image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
+            containers: [containerTemplate(name: "${nameImage}", image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
                      envVars: [
-                             [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']])]],
+                             [key: 'MAVEN_OPTS', value: '-Duser.home=/root/']])],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) 
     {
